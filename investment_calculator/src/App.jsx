eb788c551498c0b8ad23f94cf1ba73e53e119857
IvanTsvetkov <tsvetkovivan365@gmail.com>
2025-09-28 02:42:53 +0300

    Commiting investment_calculator_project

---
+++ investment_calculator/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header title={'Investment Calculator'} />
+      {/* Other components will go here */}
+    </div>
+  );
+}
+
+export default App;
---
+++ investment_calculator/src/components/Output.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { calculateInvestmentResults } from '../util/investments.js';
+
+const Output = () => {
+}
+
+export default Output
