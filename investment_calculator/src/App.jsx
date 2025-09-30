83ababb1521df7b93e2a2c094bcf042d8fbdd5dd
IvanTsvetkov <tsvetkovivan365@gmail.com>
2025-09-30 15:06:48 +0300

    Added fields for user input fields and reset button in investment_calculator

--- investment_calculator/src/App.jsx
+++ investment_calculator/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
+import UserInput from './components/UserInput';
 
 function App() {
   return (
     <div>
       <Header title={'Investment Calculator'} />
-      {/* Other components will go here */}
+      <UserInput />
     </div>
   );
 }
--- investment_calculator/src/components/Output.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { calculateInvestmentResults } from '../util/investments.js';
-
-const Output = () => {
-}
-
-export default Output
