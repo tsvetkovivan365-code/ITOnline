189dad6af1c21ef15f9d5fbad2dd2b4d41f9efa4
IvanTsvetkov <tsvetkovivan365@gmail.com>
2025-10-03 19:15:34 +0300

    Adding Output.jsx to investment_calculator + updated index.css and App.jsx

--- investment_calculator/src/App.jsx
+++ investment_calculator/src/App.jsx
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import UserInput from './components/UserInput';
+import OutputData from './components/Output';
+import { calculateInvestmentResults } from './util/investments';
+import { generatepdf } from './util/generatereport';
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 4000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+  });
+
+  const resData = calculateInvestmentResults(userInput);
+
+  const handleGeneratePDF = () => {
+    generatepdf({...userInput, results: resData});
+  }
+
+  const userInputValidation = userInput.duration >= 1;
+    
+  const handleInputChange = (inputIdentifier, newValue) => {
+    setUserInput(prevInput => ({
+      ...prevInput,
+      [inputIdentifier]: +newValue
+    }));
+  };
+
   return (
-    <div>
-      <Header title={'Investment Calculator'} />
-      <UserInput />
-    </div>
+    <>
+      <Header />
+      <UserInput userInput={userInput} onInputChange={handleInputChange} setUserInput={setUserInput}/>
+      {!userInputValidation && <p>Please ensure that the years invested are greater than 0!</p>}
+      {userInputValidation && <OutputData inputValue={userInput} />}
+      <div className='generate-report'>
+        <button onClick={handleGeneratePDF}>Download report</button>
+      </div>
+    </>
   );
 }
 
