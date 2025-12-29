13d6df0c08f849d9421bd9fd3ee26af1bfe50210
IvanTsvetkov <tsvetkovivan365@gmail.com>
2025-12-29 13:37:11 +0200

    Adding Game top component

--- tic_tac_toe/tic_tac_toe/src/App.jsx
+++ tic_tac_toe/tic_tac_toe/src/App.jsx
@@ -1,6 +1,21 @@
 import { useState } from "react";
 
-export default function Board() {
+
+export default function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
+}
+
+
+function Board() {
   const [xIsNext, setXIsNext] =useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
