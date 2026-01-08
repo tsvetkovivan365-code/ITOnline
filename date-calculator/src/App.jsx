d184a1290dfbc96ab4fe076991e8cb59dea02e52
IvanTsvetkov <tsvetkovivan365@gmail.com>
2026-01-08 18:21:42 +0200

    Adding date logic for calculating date

--- date-calculator/src/App.jsx
+++ date-calculator/src/App.jsx
@@ -10,13 +10,25 @@ export default function App() {
 function Counter() {
   const [count, setCount] = useState(0);
 
+  const date = new Date();
+
+  date.setDate(date.getDate() + count);
+
   return(
     <div>
       <div>
         <button onClick={() => setCount(c => c - 1)}>-</button>
-        <span>Count:{count}</span>
+        <span> Count: {count} </span>
         <button onClick={() => setCount(c => c + 1)}>+</button>
       </div>
+      <p>
+        <span>{count === 0 
+          ? "Today is " 
+          : count > 0 
+          ? `${count} days from today is ` 
+          : `${Math.abs(count)} days ago was `}</span>
+        <span>{date.toDateString()}</span>
+      </p>
     </div>
   );
 }
